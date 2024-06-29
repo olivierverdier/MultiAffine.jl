b56888f0ecdefc531f5bfca44d7f1d57f06f6ab6
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-29 15:56:06 +0200

    clean inv_diff test

--- test/multiaffine/inv_diff.jl
+++ test/multiaffine/inv_diff.jl
@@ -6,14 +6,13 @@ rng = Random.default_rng()
 
 
 
-test_inv_diff_set(G, χ, ξ) = @testset "inv_diff" for side in [LeftSide(), RightSide()]
-    @test GroupTools.check_inv_diff(G, χ, ξ, side)
-end
 
 
 @testset "inv_diff" begin
-    G = MultiDisplacement(3,2)
+    G = MultiDisplacement(3, 2)
     χ = rand(rng, G)
     ξ = rand(rng, TangentSpace(G, identity_element(G)))
-    test_inv_diff_set(G, χ, ξ)
+    @testset "$side" for side in [LeftSide(), RightSide()]
+        @test GroupTools.check_inv_diff(G, χ, ξ, side)
+    end
 end
