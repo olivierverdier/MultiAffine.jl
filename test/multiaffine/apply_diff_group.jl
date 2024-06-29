c24797e0ca3aa9fa24fcde9bcd6d7764c1147d5b
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-29 15:55:53 +0200

    clean apply_diff_group test

--- test/multiaffine/apply_diff_group.jl
+++ test/multiaffine/apply_diff_group.jl
@@ -6,18 +6,13 @@ rng = Random.default_rng()
 
 
 
-test_apply_diff_group_set(G, χ, ξ, χ_) = begin
-    @testset "$dir, $side" for side in [LeftSide(), RightSide()], dir in [LeftAction(), RightAction()]
-            @test GroupTools.check_apply_diff_group(GroupOperationAction(G, (dir, side)), χ, ξ, χ_)
-    end
-end
-
-
 @testset "apply_diff_group" begin
-    G = MultiDisplacement(3,2)
+    G = MultiDisplacement(3, 2)
     # χ1 = rand(rng, G)
     χ1 = Identity(G)
     χ2 = rand(rng, G)
     ξ = rand(rng, TangentSpace(G, identity_element(G)))
-    test_apply_diff_group_set(G, χ1, ξ, χ2)
+    @testset "$dir, $side" for side in [LeftSide(), RightSide()], dir in [LeftAction(), RightAction()]
+            @test GroupTools.check_apply_diff_group(GroupOperationAction(G, (dir, side)), χ1, ξ, χ2)
+    end
 end
