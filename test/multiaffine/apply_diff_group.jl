0ffadc7955d680a2312f9e34f79812516fd974c7
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-30 14:34:24 +0200

    Better use of `check_apply_diff_group`

--- test/multiaffine/apply_diff_group.jl
+++ test/multiaffine/apply_diff_group.jl
@@ -5,13 +5,15 @@ rng = Random.default_rng()
 
 
 
-@testset "apply_diff_group" begin
-    G = MultiDisplacement(3, 2)
-    # χ1 = rand(rng, G)
-    χ1 = Identity(G)
+@testset "apply_diff_group $G" for
+    G in [MultiDisplacement(3, 2), MultiDisplacement(2)]
+    χ1 = rand(rng, G)
     χ2 = rand(rng, G)
     ξ = rand(rng, TangentSpace(G, identity_element(G)))
-    @testset "$dir, $side" for side in [LeftSide(), RightSide()], dir in [LeftAction(), RightAction()]
+    @testset "$χ $dir $side" for
+        χ in [χ1, Identity(G)],
+        side in [LeftSide(), RightSide()],
+        dir in [LeftAction(), RightAction()]
             @test GT.check_apply_diff_group(GroupOperationAction(G, (dir, side)), χ1, ξ, χ2)
     end
 end
