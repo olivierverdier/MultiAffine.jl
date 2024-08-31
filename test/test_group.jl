9bf5893c8ebdd6b312e3636b0e8ac3c0baf06941
Olivier Verdier <olivier.verdier@gmail.com>
2024-08-31 02:59:47 +0200

    TST: exp/log tests

--- test/test_group.jl
+++ test/test_group.jl
@@ -172,6 +172,18 @@ end
 end
 
 
+@testset for G in [MultiDisplacement(3,2)]
+    χ1, χ2 = [rand(rng, G) for i in 1:2]
+    ξ1, ξ2 = [rand_lie(rng, G) for i in 1:2]
+    v1 = translate_from_id(G, χ1, ξ1, LeftSide())
+    @test GT.check_exp_invariant(G, exp, χ1, v1, χ2)
+    @test GT.check_exp_log(G, exp, log, χ1, χ2)
+    @test GT.check_log_exp(G, log, exp, χ1, v1)
+    v = similar(v1)
+    @test GT.check_log_log_(G, log, log!, v, χ1, χ2)
+    χ = similar(χ1)
+    @test GT.check_exp_exp_(G, exp, exp!, χ, χ1, v1)
+end
 
 
 
